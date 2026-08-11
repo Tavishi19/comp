de14b9aebf6db4bd0eeac5a69ee8ead3dddb5cad
Tavishi19 <tavishi.raikar@gmail.com>
2026-08-11 22:55:00 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Explore button
+document.querySelector(".explore-btn").addEventListener("click", function () {
+    document.querySelector(".bottom-panel").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+
+// Explore Initiatives button
+document.querySelector(".primary-btn").addEventListener("click", function () {
+    document.querySelector(".bottom-panel").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+
+// Watch Overview button
+document.querySelector(".secondary-btn").addEventListener("click", function () {
+    alert("Overview coming soon!");
+});
